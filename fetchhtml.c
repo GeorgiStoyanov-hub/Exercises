ade76bd0fec981b2be75b9861285ffbdc28ec7c1
Georgi Stoyanov <153766792+GeorgiStoyanov-hub@users.noreply.github.com>
2024-11-20 18:27:39 +0200

    Update fetchhtml.c
    Added IGNORE flag to disable SSL among other things.

--- fetchhtml.c
+++ fetchhtml.c
@@ -1,38 +1,33 @@
-/* This is a script that fetches HTML code from HTTP websites
-*  It's written using the libcurl library
-*  for more information, refer to https://curl.se/
-*  This is completely for educational purposes only.
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <curl/curl.h>
 
+#define IGNORE_ARG "-IGNORE"
 
 int main(int argc, char* argv[]) 
 {   
 
-    if(argc > 3)
+    if(argc > 4)
     {
-        fprintf(stderr, "\nUsage: %s <URL> <Output>\n", argv[0]);
-        fprintf(stderr, "\nError 1 : Too many arguments passed\n");
+        fprintf(stderr, "\nUsage: %s <URL> <Output> (Arguments)\n", argv[0]);
+        fprintf(stderr, "Error 1 : Too many arguments passed\n");
         return EXIT_FAILURE;
     }
-    if(argc != 3)
+    if(argc < 3)
     {
-        fprintf(stderr, "\nUsage: %s <URL> <Output>\n", argv[0]);
-        fprintf(stderr, "\nError 2 : Must include 3 arguments\n");
+        fprintf(stderr, "\nUsage: %s <URL> <Output> (Arguments)\n", argv[0]);
+        fprintf(stderr, "Error 2 : Must include 3 arguments\n");
         return EXIT_FAILURE;
     }
 
     const char* URL = argv[1];
     const char* html_output = argv[2];
-
     CURL *curl = curl_easy_init();
 
     if(!curl) 
     {
-        fprintf(stderr, "failed to initialize", curl_easy_strerror);
+        fprintf(stderr, "\nfailed to initialize\n", curl_easy_strerror);
         return EXIT_FAILURE;
     }
 
@@ -40,7 +35,7 @@ int main(int argc, char* argv[])
 
     if(!fl)
     {
-        perror("Error opening file.");
+        perror("\nError opening file.\n");
         curl_easy_cleanup(curl);
         return EXIT_FAILURE;
     }
@@ -48,17 +43,41 @@ int main(int argc, char* argv[])
 
     //setoptions
     curl_easy_setopt(curl, CURLOPT_URL, URL);
-    printf("Fetching URL: %s\n", URL);
+    printf("\nFetching URL: %s\n", URL);
+
+    char* arguments = NULL;
+    if(argc == 4)
+    {
+        arguments = argv[3];
+        if (strcmp(arguments, IGNORE_ARG) == 0)
+        {
+            curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+            curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+            printf("SSL verification is disabled.\n");
+        }
+        else
+        {
+            fprintf(stderr, "Error 3 : Invalid Argument!\n");
+            fclose(fl);
+            curl_easy_cleanup(curl);
+            return EXIT_FAILURE;
+        }
+    }
+
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, fl);
     printf("Saving to file: %s\n", html_output);
+
     //perform
     CURLcode result = curl_easy_perform(curl);
+
     if(result != CURLE_OK)
     {
         fprintf(stderr, "cURL request failed %s\n", curl_easy_strerror(result));
+        fclose(fl);
         curl_easy_cleanup(curl);
         return EXIT_FAILURE;
     }
+
     printf("HTML content successfully saved to '%s'.\n", html_output);
     long http_code = 0;
     curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
@@ -72,7 +91,7 @@ int main(int argc, char* argv[])
     {
         printf("Request was successful!\n");
     }
-
+    //
     curl_easy_cleanup(curl);
     fclose(fl);
     return result == CURLE_OK ? EXIT_SUCCESS : EXIT_FAILURE; 
